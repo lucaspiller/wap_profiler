056990dfa74d8eafa5f1c87216e3d32a0cbf0791
Luca Spiller <luca@stackednotion.com>
2010-05-05 20:08:52 +0100

    Add UaProf#process! method

--- app/models/ua_prof.rb
+++ app/models/ua_prof.rb
@@ -1,3 +1,7 @@
+require 'lib/rdf_document'
+
+SCREEN_SIZE_XPATH = '//rdf:Description[@rdf:ID="HardwarePlatform"]/prf:ScreenSize'
+
 class UaProf < Ohm::Model
   attribute :uri
   attribute :status
@@ -11,8 +15,29 @@ class UaProf < Ohm::Model
     status.nil? || status == 'pending'
   end
 
+  def process!
+    begin
+      doc = RdfDocument.new(self.uri)
+
+      # screen size
+      doc.tag(SCREEN_SIZE_XPATH) =~ /([0-9]+)x([0-9]+)/
+      self.width = $1
+      self.height = $2
+
+      # update status and save
+      self.status = 'processed'
+      save
+    rescue Nokogiri::XML::SyntaxError => e
+      # parse error
+      # TODO: Log it, and pos. ignore it in the future
+      false
+    end
+  end
+
   def validate
     assert_present :uri
+
+    # set default status
     if status.nil?
       self.status = 'pending'
     end
